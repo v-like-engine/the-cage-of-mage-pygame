45143fbcc157394abfe931762ec6f94f85cf3fba
v-like-engine <cypepv@yandex.ru>
2020-01-08 18:48:02 +0500

    Remake of self.v (speed) variable, now correctly works with borders

--- hero_classes.py
+++ hero_classes.py
@@ -30,10 +30,11 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
 
 class Mage(AnimatedSprite):
-    def __init__(self, x, y, box_group):
+    def __init__(self, x, y, fps, box_group):
         super().__init__(load_image('mage pictures.png'), 8, 4, x, y)
         self.velocity = [0, 0]
         self.direction = 0
+        self.v = 180 / fps
         self.g = 1
         self.up = False
         self.box_group = box_group
@@ -69,22 +70,24 @@ class Mage(AnimatedSprite):
                 if self.chest:
                     if frame in [15, 31] or pygame.sprite.spritecollideany(self, borders[1]) or \
                             pygame.sprite.collide_mask(self, self.chest):
-                        self.change_coords(0, -10)
+                        self.velocity = -self.velocity[0], self.velocity[1]
+                        self.change_coords(0)
                 else:
                     if frame in [15, 31] or pygame.sprite.spritecollideany(self, borders[1]):
-                        self.change_coords(0, -10)
+                        self.velocity = -self.velocity[0], self.velocity[1]
+                        self.change_coords(0)
             # self.move(self.x, self.y + 10 / FPS)
         if event.type == pygame.KEYUP:
             self.up = False
 
-    def change_coords(self, x_or_y, step, *g):
+    def change_coords(self, x_or_y, *g):
         if x_or_y == 0:
-            self.rect.x += step * self.velocity[0]
-            self.rect.y += step * self.velocity[1]
+            self.rect.x += self.v * self.velocity[0]
+            self.rect.y += self.v * self.velocity[1]
         elif x_or_y == 1:
-            self.rect.x += step * self.velocity[0]
+            self.rect.x += self.v * self.velocity[0]
         else:
-            self.rect.y += step * self.velocity[1] * g[0]
+            self.rect.y += self.v * self.velocity[1] * g[0]
         self.x = self.rect.x
         self.y = self.rect.y
 
