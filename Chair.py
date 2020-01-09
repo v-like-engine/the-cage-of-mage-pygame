53a5aa25478da8e3e7d350a65b4fc3a1dd9c2dc9
mihdenis85 <mihdenis777@yandex.ru>
2020-01-09 20:48:48 +0500

    Add chair

---
+++ Chair.py
@@ -0,0 +1,13 @@
+import pygame
+
+from load_image import load_image
+from simple_sprite import SimpleSprite
+
+
+class Chair(SimpleSprite):
+    def __init__(self, group, screen, x, y):
+        super().__init__(group, screen, x, y)
+        self.image = load_image('chair.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
